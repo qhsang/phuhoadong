de0d08d242bffa813da82abc0bec6cd8bd939bd8
qhsang <105709672+qhsang@users.noreply.github.com>
2025-07-21 21:21:52 +0700

    update information

--- src/data/coQuan.ts
+++ src/data/coQuan.ts
@@ -14,39 +14,40 @@ export interface CoQuan {
 export const coQuan: CoQuan[] = [
   {
     id: 1,
-    tieuDe: "TRỤ SỞ: ĐẢNG UỶ - UỶ BAN MTTQ VIỆT NAM XÃ",
+    tieuDe: "ĐẢNG UỶ- UỶ BAN MTTQ VIỆT NAM XÃ PHÚ HOÀ ĐÔNG",
     diaChi: "463 Tỉnh lộ 15, xã Phú Hoà Đông, TP.HCM",
     soDienThoai: "0283.8927.750",
     nhanSu: [
-      { chucVu: "Bí thư", hoTen: "Nguyễn Văn Nghĩa" },
-      { chucVu: "Phó Bí Thư", hoTen: "Lê Thị Ánh Tuyết" },
-      { chucVu: "Chánh Văn phòng Đảng uỷ", hoTen: "Lê Thị Minh Đan" },
-      { chucVu: "Trưởng Ban Xây dựng Đảng", hoTen: "Võ Thị Thanh Hằng" },
-      { chucVu: "Chủ nhiệm UBKT", hoTen: "Phan Võ Thuý Phượng" },
-      { chucVu: "Chủ tịch Uỷ ban MTTQ Việt Nam xã", hoTen: "Nguyễn Thị Thuận" },
-      { chucVu: "PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Hội Liên hiệp phụ nữ xã", hoTen: "Trần Thị Thanh Huyền" },
-      { chucVu: "PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Hội Cựu Chiến binh xã", hoTen: "Lương Đức Toản" },
-      { chucVu: "PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Hội Nông dân xã", hoTen: "Trương Minh Phương" },
-      { chucVu: "PCT. Uỷ ban MTTQ đồng thời là Bí Thư Đoàn xã", hoTen: "Trần Đỗ Xuân Thương" },
-      { chucVu: "PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Công đoàn xã", hoTen: "Nguyễn Huỳnh Quang" }
+      { chucVu: "Bí thư Đảng uỷ xã", hoTen: "Nguyễn Văn Nghĩa" },
+      { chucVu: "Phó Bí Thư Thường trực Đảng uỷ xã", hoTen: "Lê Thị Ánh Tuyết" },
+      { chucVu: "UVBTV/Trưởng Ban Xây dựng Đảng Đảng uỷ xã", hoTen: "Võ Thị Thanh Hằng" },
+      { chucVu: "UVBTV/Chủ nhiệm UBKT Đảng uỷ xã", hoTen: "Phan Võ Thuý Phượng" },
+      { chucVu: "UVBTV/Chủ tịch Uỷ ban MTTQ Việt Nam xã", hoTen: "Nguyễn Thị Thuận" },
+      { chucVu: "UVBTV/Chỉ huy trưởng Ban CHQS xã", hoTen: "Lê Văn Ty" },
+      { chucVu: "ĐUV/Chánh Văn phòng Đảng uỷ xã", hoTen: "Lê Thị Minh Đan" },
+      { chucVu: "ĐUV/PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Hội Liên hiệp phụ nữ xã", hoTen: "Trần Thị Thanh Huyền" },
+      { chucVu: "ĐUV/PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Hội Cựu Chiến binh xã", hoTen: "Lương Đức Toản" },
+      { chucVu: "ĐUV/PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Hội Nông dân xã", hoTen: "Trương Minh Phương" },
+      { chucVu: "ĐUV/PCT. Uỷ ban MTTQ đồng thời là Bí Thư Đoàn xã", hoTen: "Trần Đỗ Xuân Thương" },
+      { chucVu: "ĐUV/PCT. Uỷ ban MTTQ đồng thời là Chủ tịch Công đoàn xã", hoTen: "Nguyễn Huỳnh Quang" }
     ]
   },
   {
     id: 2,
-    tieuDe: "TRỤ SỞ: HỘI ĐỒNG NHÂN DÂN - UỶ BAN NHÂN DÂN",
+    tieuDe: "HỘI ĐỒNG NHÂN DÂN – UỶ BAN NHÂN DÂN (Trung tâm phục vụ hành chính công)",
     diaChi: "469 Tỉnh lộ 8, xã Phú Hoà Đông, TP.HCM",
     soDienThoai: "0283.7950.793",
     nhanSu: [
-      { chucVu: "Chủ tịch Hội đồng nhân dân", hoTen: "Nguyễn Văn Nghĩa" },
-      { chucVu: "Phó Chủ tịch Hội đồng nhân dân", hoTen: "Đỗ Thị Thanh Thuý" },
-      { chucVu: "Trưởng ban Văn hoá – Xã hội HĐND", hoTen: "Võ Thị Thanh Hằng" },
-      { chucVu: "Trưởng ban Kinh tế - Ngân sách HĐND", hoTen: "Phan Võ Thuý Phượng" },
-      { chucVu: "Chủ tịch UBND", hoTen: "Nguyễn Minh Tuấn" },
-      { chucVu: "Phó Chủ tịch UBND đồng thời là Giám đốc Trung Tâm Phục vụ Hành Chính Công", hoTen: "Trần Tuấn Khanh" },
-      { chucVu: "Phó Chủ tịch UBND", hoTen: "Đinh Hoài Phong" },
-      { chucVu: "Chánh văn phòng HĐND – UBND", hoTen: "Lê Hoàng Nguyên" },
-      { chucVu: "Trưởng phòng Kinh tế", hoTen: "Nguyễn Thị Đẹp" },
-      { chucVu: "Trưởng phòng Văn hoá – Xã hội", hoTen: "Nguyễn Văn Thuận" }
+      { chucVu: "Bí thư Đảng uỷ/Chủ tịch Hội đồng nhân dân xã", hoTen: "Nguyễn Văn Nghĩa" },
+      { chucVu: "UVBTV/Phó Chủ tịch Hội đồng nhân dân xã", hoTen: "Đỗ Thị Thanh Thuý" },
+      { chucVu: "UVBTV/Trưởng ban Văn hoá – Xã hội HĐND xã", hoTen: "Võ Thị Thanh Hằng" },
+      { chucVu: "UVBTV/Trưởng ban Kinh tế - Ngân sách HĐND xã", hoTen: "Phan Võ Thuý Phượng" },
+      { chucVu: "Phó bí thư/Chủ tịch UBND xã", hoTen: "Nguyễn Minh Tuấn" },
+      { chucVu: "UVBTV/Phó Chủ tịch UBND đồng thời là Giám đốc Trung Tâm Phục vụ Hành Chính Công", hoTen: "Trần Tuấn Khanh" },
+      { chucVu: "ĐUV/Phó Chủ tịch UBND", hoTen: "Đinh Hoài Phong" },
+      { chucVu: "ĐUV/Trưởng phòng Kinh tế", hoTen: "Nguyễn Thị Đẹp" },
+      { chucVu: "ĐUV/Trưởng phòng Văn hoá – Xã hội", hoTen: "Nguyễn Văn Thuận" },
+      { chucVu: "Chánh văn phòng HĐND – UBND", hoTen: "Lê Hoàng Nguyên" }
     ]
   },
   {
@@ -55,7 +56,7 @@ export const coQuan: CoQuan[] = [
     diaChi: "181 Tỉnh lộ 15, xã Phú Hoà Đông, TP.HCM",
     soDienThoai: "0283.8927.048",
     nhanSu: [
-      { chucVu: "Trưởng Công an", hoTen: "Nguyễn Văn Tâm" },
+      { chucVu: "UVBTV/Trưởng Công an xã", hoTen: "Nguyễn Văn Tâm" },
       { chucVu: "Phó Trưởng Công an", hoTen: "Nguyễn Duy Nhất" },
       { chucVu: "Phó Trưởng Công an", hoTen: "Nguyễn Ngọc Tú" },
       { chucVu: "Phó Trưởng Công an", hoTen: "Nguyễn Thành Hôn" },
